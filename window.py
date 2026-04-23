bb554371ff318790d3717d003303124b77d03c2c
Simineon <tyulenev11@internet.ru>
2026-04-23 14:26:34 +0500

    Update window.py

--- window.py
+++ window.py
@@ -2,21 +2,9 @@ from PyQt6.QtWidgets import QMainWindow, QMdiArea, QMdiSubWindow
 from app import AppWidget
 from terminal_emulator import TerminalWidget
 
-
 class MainWindow(QMainWindow):
-
     def __init__(self):
         super().__init__()
         widget = AppWidget()
         widget.resize(800, 600)
         widget.show()
-
-        mdi = QMdiArea()
-        terminal_window = QMdiSubWindow()
-        term_widget = TerminalWidget()
-        terminal_window.setWidget(term_widget)
-        mdi.addSubWindow(terminal_window)
-        terminal_window.show()
-
-
-        self.setCentralWidget(mdi)
