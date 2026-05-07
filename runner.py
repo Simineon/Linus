663b4675a7ae6a6f869038edc6bf0923c323ebf6
Simineon <tyulenev11@internet.ru>
2026-05-07 14:16:05 +0500

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,17 +1,22 @@
 import subprocess
+from locale import delocalize
+
 
 class Runner:
     def __init__(self, file):
         self.file = file
+        self.output = ""
 
     def runPython(self):
         try:
             if self.file:
-                self.data = subprocess.run(["python", self.file])
+                self.data = subprocess.run(["python", self.file], capture_output=True, text=True)
+                self.output = self.data.stdout
             else:
                 self.data = subprocess.run(["python3", self.file])
+                self.output = self.data.stdout
         except Exception as e:
             print(e)
 
-    def get_output_data(self):
-        data = self.data.stdout
+    def get_output(self):
+        return self.output
